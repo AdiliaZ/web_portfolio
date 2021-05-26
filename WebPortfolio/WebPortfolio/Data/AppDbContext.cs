878cdf1ca887c1276f68b72d13b65932104692d5
AdiliaZ <65673503+AdiliaZ@users.noreply.github.com>
2021-05-26 14:13:45 +0300

    Identity & Admin

--- WebPortfolio/WebPortfolio/Data/AppDbContext.cs
+++ WebPortfolio/WebPortfolio/Data/AppDbContext.cs
@@ -1,13 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebPortfolio.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace WebPortfolio.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
         {
